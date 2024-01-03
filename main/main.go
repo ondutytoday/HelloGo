335812348c4c0c613bab9d76c4288e2e8a1c3eb2
Elena Vasilyeva <elena.vasilyeva@piano.io>
2024-01-03 22:21:10 +0400

    functions with multiple return

--- main/main.go
+++ main/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"HelloGo/anotherfile"
-	"HelloGo/morestrings"
 	"fmt"
-	"github.com/google/go-cmp/cmp"
-	"rsc.io/quote"
+	"strconv"
 )
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func namedReturn(x, y int) (a int, b string) {
+	a = x
+	b = "string named: " + strconv.Itoa(y) //converts int value to string
+	return
+}
+
 func main() {
-	hello := "Hello world!"
-	fmt.Println(hello)
-	fmt.Println(quote.Go())
-	fmt.Println(anotherfile.Version())
-	fmt.Println(morestrings.ReverseRunes("some string"))
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	//hello := "Hello world!"
+	//fmt.Println(hello)
+	//fmt.Println(quote.Go())
+	//fmt.Println(anotherfile.Version())
+	//fmt.Println(morestrings.ReverseRunes("some string"))
+	//fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fmt.Println(swap("a", "b"))
+	a, b := namedReturn(3, 5)
+	fmt.Printf("return a: %d\nreturn b: %s\n", a, b)
+	s := string(1024) //converts bytes to char
+	fmt.Println(s)
 }
