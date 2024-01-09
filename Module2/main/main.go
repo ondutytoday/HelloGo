d6fc84174e7626e4b96c589b9730f6be271edd3e
Elena Vasilyeva <elena.vasilyeva@piano.io>
2024-01-09 19:50:41 +0400

    collections

--- Module2/main/main.go
+++ Module2/main/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"Module2/collections"
 	"Module2/pointers"
 	"Module2/structs"
 	"fmt"
 )
 
 func main() {
-	pointers.PrintPointers()
-	structs.Method()
-	var m structs.MyStructure
-	m.X = 4
-	// m.y = 1 - недоступно т.к с маленькой буквы -> private
-	fmt.Println(m)
+	if true {
+		collections.Arrays()
+		collections.Slices()
+	} else {
+		pointers.PrintPointers()
+		structs.Method()
+		var m structs.MyStructure
+		m.X = 4
+		// m.y = 1 - недоступно т.к с маленькой буквы -> private
+		fmt.Println(m)
+	}
 }
